f1b95d92f5d4036579d8b21c28e88590889fa654
Rafael Lasso <rafael.comercial27@gmail.com>
2022-07-01 16:07:47 -0300

    Lading page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+// from react
+import React from 'react';
+
+
+// about project
 import './App.css';
 import Navbar from './assets/layout/Navbar';
+import developer from './assets/imgs/developer-activity-animate.svg'
+
 
 function App() {
   return (
     <>
       <Navbar/>
+        <section className='seeMore'>
+          <h1>Ola eu sou <span>Rafael Lasso!</span></h1>
+          <img className='developer' src={developer}/>
+        </section>
     </>
   );
 }
